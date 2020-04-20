e43d5bb2e8086eb845da650297276b8020d1a232
Marina <marishkina@gmail.com>
2020-04-20 16:32:04 +0800

    добавляет картинки webp в css, concat js

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require("gulp-svgstore")
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var concat = require('gulp-concat');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -27,6 +28,18 @@ gulp.task("css", function () {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('source/js/main/*.js')
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('scripts-v', function () {
+  return gulp.src('source/js/vendor/*.js')
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('build/js'));
 });
 
 gulp.task("server", function () {
@@ -85,7 +98,6 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
     "source//*.ico"
     ], {
       base: "source"
@@ -97,5 +109,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", 'scripts', 'scripts-v', "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
