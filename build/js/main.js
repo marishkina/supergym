22722efb17bce904efb711b49649b13043c1c153
Marina <marishkina@gmail.com>
2020-04-28 23:54:56 +0800

    маска и проверка телефона

--- build/js/main.js
+++ build/js/main.js
@@ -144,7 +144,25 @@
 'use strict';
 
 (function () {
-  $(document).ready(function () {
-    $('#tel').mask('+7 (999) 999 99 99');
-  });
+  var feedback = document.querySelector('.feedback');
+
+  if (feedback) {
+    jQuery.validator.addMethod('checkMask', function (value, element) {
+      return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
+    });
+
+    $('feddback').validate({
+      rules: {
+        tel: {
+          checkMask: true
+        }
+      },
+      messages: {
+        tel: {
+          checkMask: 'Введите полный номер телефона'
+        }
+      }
+    });
+    $('#tel').mask('+7(999)999-9999', {autoclear: false});
+  }
 })();
