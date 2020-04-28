92fed406d6af434417c76df657e2fdc0f3b913a4
Marina <marishkina@gmail.com>
2020-04-28 20:31:37 +0800

    правки по баглисту, помимо телефона в форме

--- source/js/main/review-slider.js
+++ source/js/main/review-slider.js
@@ -3,35 +3,60 @@
 (function () {
   var review = document.querySelector('.reviews');
 
-  if (review) {
-    var line = review.querySelector('.reviews__list');
-    var slides = review.querySelectorAll('.reviews__item');
-    var sliderWidth = review.querySelector('.reviews__list-wrap').offsetWidth;
+  function asArr(arr) {
+    return [].slice.call(arr, 0);
+  }
 
+  function setActiveReview(reviewItems, activeReviewItem) {
+    reviewItems.forEach(function (reviewItem) {
+      if (reviewItem === activeReviewItem) {
+        reviewItem.classList.add('reviews__item--active');
+      } else {
+        reviewItem.classList.remove('reviews__item--active');
+      }
+    });
+  }
+
+  function getNextReview(reviewItems, reviewItem) {
+    var reviewIndex = reviewItems.indexOf(reviewItem);
+    var totalReviewsIndexes = reviewItems.length - 1;
+    if (reviewIndex === totalReviewsIndexes) {
+      return reviewItems[0];
+    } else {
+      return reviewItems[reviewIndex + 1];
+    }
+  }
+
+  function getPrevReview(reviewItems, reviewItem) {
+    var reviewIndex = reviewItems.indexOf(reviewItem);
+    var totalReviewsIndexes = reviewItems.length - 1;
+    if (reviewIndex === 0) {
+      return reviewItems[totalReviewsIndexes];
+    } else {
+      return reviewItems[reviewIndex - 1];
+    }
+  }
+
+  if (review) {
+    var ACTIVE_REVIEW_DEFAULT_INDEX = 0;
+    var reviewItems = asArr(review.querySelectorAll('.reviews__item'));
     var nextButton = review.querySelector('.reviews__controler--next');
     var prevButton = review.querySelector('.reviews__controler--prev');
 
-    if (sliderWidth >= 566) {
-      var width = 566;
-    } else if (sliderWidth >= 560 && sliderWidth <= 566) {
-      width = 560;
-    } else {
-      width = 226;
-    }
+    var currentlyActiveReview = reviewItems[ACTIVE_REVIEW_DEFAULT_INDEX];
 
-    var count = 1;
-    var position = 0;
-
-    prevButton.addEventListener('click', function () {
-      position += width * count;
-      position = Math.min(position, 0);
-      line.style.marginLeft = position + 'px';
-    });
+    setActiveReview(reviewItems, currentlyActiveReview);
 
     nextButton.addEventListener('click', function () {
-      position -= width * count;
-      position = Math.max(position, -width * (slides.length - count));
-      line.style.marginLeft = position + 'px';
+      var nextReview = getNextReview(reviewItems, currentlyActiveReview);
+      setActiveReview(reviewItems, nextReview);
+      currentlyActiveReview = nextReview;
+    });
+
+    prevButton.addEventListener('click', function () {
+      var prevReview = getPrevReview(reviewItems, currentlyActiveReview);
+      setActiveReview(reviewItems, prevReview);
+      currentlyActiveReview = prevReview;
     });
   }
 })();
